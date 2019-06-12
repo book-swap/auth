0987ae9914ebea7ba2a0277acbc5ef80e7758544
Mihai Voinea <hi@voinea.me>
2019-06-12 23:53:21 +0300

    Added login test

--- test/login.test.js
+++ test/login.test.js
@@ -5,22 +5,54 @@ const { expect } = require('chai');
 const app = require('../src/app');
 const User = require('../src/models/user.model');
 
-describe('POST /local/register', () => {
+describe("'auth' test suite", () => {
   const userData = {
     email: 'test_user@bookswap.ro',
     password: 'test_password'
   };
 
-  it('should create a new user', done => {
-    request(app)
-      .post('/local/register')
-      .send(userData)
-      .end(async (err, res) => {
-        expect(err).to.be.null;
-        expect(res.statusCode).to.equal(200);
-        const user = await User.find({ email: userData.email });
-        expect(user[0]).to.exist;
-        done();
-      });
+  afterEach(() => {
+    User.deleteMany({}).catch(error => console.log(error));
+  });
+
+  describe('POST /local/register', () => {
+    it('should create a new user', done => {
+      request(app)
+        .post('/local/register')
+        .send(userData)
+        .end(async (err, res) => {
+          expect(err).to.be.null;
+          expect(res.statusCode).to.equal(200);
+          const user = await User.find({ email: userData.email });
+          expect(user[0]).to.exist;
+          done();
+        });
+    });
+  });
+
+  describe('POST /local/login', () => {
+    it('should login a user', done => {
+      User.create(userData)
+        .then(createdUser => {
+          request(app)
+            .post('/local/login')
+            .send(userData)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res.statusCode).to.equal(200);
+              expect(res.body.data.email).to.equal(createdUser.email);
+              expect(res.body.data.email).to.equal(userData.email);
+              done();
+            });
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+
+    it('should return a valid JWT', done => {
+      // TODO
+      done();
+    });
   });
 });
