1c9c5ece81ffe3797a734427a884f8e30dc6e801
Mihai Voinea <hi@voinea.me>
2019-07-25 15:36:21 +0300

    JWT sign function changes for compatibility with user service

--- src/helpers/jsonwebtoken.helper.js
+++ src/helpers/jsonwebtoken.helper.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 exports.generateJWTFromUser = user =>
   jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-    issuer: 'BookSwap',
+    issuer: 'auth.bookswap',
+    audience: 'BookSwap',
     subject: user.id,
     expiresIn: '24h',
     algorithm: 'HS256'
